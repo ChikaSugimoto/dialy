e824b1a4f17bb35e0eab20402aa20527ae0b48ae
Chika Sugimoto <chika@ChikanoMacBook-ea.local>
2019-02-11 15:07:49 +0900

    make toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   resources :diaries
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
-  get '/users/new'
+  root 'pages#index'
 end
